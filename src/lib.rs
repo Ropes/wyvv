e340310c7ca36d200019bdf18c5d0eb660910514
Josh Roppo <joshroppo@gmail.com>
2021-04-03 16:05:46 -0700

    Functional tests passing for NATO alphabet

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,45 @@
+#![warn(dead_code)]
 use phf::phf_map;
 
-static ALPHABET: phf::Map<&'static str, &'static str> = phf_map! {
-    "a" => "alpha",
-    "b"=> "bravo",
-    "c"=> "charlie",
-    "d"=> "delta",
-    "e"=> "echo",
-    "f"=> "foxtrot",
-    "g"=> "golf",
-    "h"=> "hotel",
-    "i"=> "india",
-    "j"=> "juliet",
-    "k"=> "kilo",
-    "l"=> "lima",
-    "m"=> "mike",
-    "n"=> "november",
-    "o"=> "oscar",
-    "p"=> "papa" ,
-    "q"=> "quebec",
-    "r"=> "romeo",
-    "s"=> "sierra",
-    "t"=> "tango",
-    "u"=> "uniform",
-    "v"=> "victor",
-    "w"=> "whiskey",
-    "x"=> "xray",
-    "y"=> "yankee",
-    "z"=> "zulu",
+static ALPHABET: phf::Map<char, &'static str> = phf_map! {
+    'a' => "alpha",
+    'b'=> "bravo",
+    'c'=> "charlie",
+    'd'=> "delta",
+    'e'=> "echo",
+    'f'=> "foxtrot",
+    'g'=> "golf",
+    'h'=> "hotel",
+    'i'=> "india",
+    'j'=> "juliet",
+    'k'=> "kilo",
+    'l'=> "lima",
+    'm'=> "mike",
+    'n'=> "november",
+    'o'=> "oscar",
+    'p'=> "papa" ,
+    'q'=> "quebec",
+    'r'=> "romeo",
+    's'=> "sierra",
+    't'=> "tango",
+    'u'=> "uniform",
+    'v'=> "victor",
+    'w'=> "whiskey",
+    'x'=> "xray",
+    'y'=> "yankee",
+    'z'=> "zulu",
 };
 
-/* 
-static DIGETS: phf::Map<, &'static str> = phf_map! {
+fn nato_alpha(c: char) -> &'static str {
+    let result = ALPHABET[&c];
+    return result;
+}
+
+/*
+static DIGITS: phf::Map<u32, &'static str> = phf_map! {
     0 => "zero",
     1 => "one",
+    2 => "two",
 };
 
 lazy_static!{
@@ -70,7 +77,16 @@ lazy_static!{
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn lookup_alpha() {
+        use crate::nato_alpha;
+        let a = 'a';
+        assert_eq!(nato_alpha(a), "alpha");
+    }
+
+    #[test]
+    fn lookup_delta(){
+        use crate::nato_alpha;
+        let a = 'd';
+        assert_eq!(nato_alpha(a), "delta");
     }
 }
